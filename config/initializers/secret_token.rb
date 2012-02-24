d472c7bc2a025a037bcc64c6202021478ee72c44
johndavid400 <johndavid400@yahoo.com>
2012-02-24 08:21:55 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhiteLabel::Application.config.secret_token = 'ab193071475fe1f57c403c678aefeb0f78af4172d3443bcdbfa56a99acd34495e020462bb933b503140260cf6da5cc6615c8c0477740ae3f58c406c30915fe95'
